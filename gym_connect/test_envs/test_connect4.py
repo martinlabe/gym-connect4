ff5b4b9b81a92b6c3160405d869f8e3bb7d415fe
Martin Labe <martin.labe@lirmm.fr>
2022-02-15 17:26:00 +0100

    [add] tests and empty tests_env

---
+++ gym_connect/test_envs/test_connect4.py
@@ -0,0 +1,33 @@
+import Connect4
+
+def test(actions, verbose=True):
+    '''test a list of instructions'''
+    a = Connec4Env({"width": 7, "height": 6, "connect": 4, "testmode": 1})
+    n = len(actions)
+    res = True
+    for i, (player, action) in enumerate(actions):
+        pos = a.play(player, action)
+        out = a.win(player, pos)
+        # intermediary step
+        if i != n - 1:
+            if out:
+                res = False
+        # final step
+        else:
+            if not out:
+                res = False
+    if verbose:
+        print(res)
+        a.print_board()
+    elif not res:
+        print(res)
+
+test([(1, 0), (1, 0), (1, 0), (1, 0)])
+test([(2, 0), (2, 0), (1, 0), (1, 0), (1, 0), (1, 0)])
+test([(2, 2), (2, 4), (2, 5), (2, 3)])
+test([(1, 0), (1, 0), (1, 0), (2, 0), (1, 1), (1, 1), (2, 1), (1, 2), (2, 2),
+    (2, 3)])
+test([(2, 3), (1, 4), (2, 4), (1, 5), (1, 5), (2, 5), (1, 6), (1, 6), (1, 6),
+    (2, 6)])
+test([(1, 1), (2, 1), (1, 2), (1, 2), (2, 2), (2, 3), (1, 3), (2, 3), (2, 3),
+    (1, 4), (2, 4), (1, 4), (1, 4), (2, 4)])
