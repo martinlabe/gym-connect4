685cf68892abe31c9f5e43259f0bf8bc72990af2
Martin Labe <martin@labe.fr>
2022-02-16 15:54:05 +0100

    [fix] the import in test

--- test_connect4/test_connect4.py
+++ test_connect4/test_connect4.py
@@ -1,8 +1,9 @@
-import Connect4
+from gym_connect4.envs.connect4_env import Connect4Env
+
 
 def test(actions, verbose=True):
-    '''test a list of instructions'''
-    a = Connec4Env({"width": 7, "height": 6, "connect": 4, "testmode": 1})
+    """test a list of instructions"""
+    a = Connect4Env({"width": 7, "height": 6, "connect": 4, "test_mode": 1})
     n = len(actions)
     res = True
     for i, (player, action) in enumerate(actions):
@@ -22,12 +23,13 @@ def test(actions, verbose=True):
     elif not res:
         print(res)
 
+
 test([(1, 0), (1, 0), (1, 0), (1, 0)])
 test([(2, 0), (2, 0), (1, 0), (1, 0), (1, 0), (1, 0)])
 test([(2, 2), (2, 4), (2, 5), (2, 3)])
 test([(1, 0), (1, 0), (1, 0), (2, 0), (1, 1), (1, 1), (2, 1), (1, 2), (2, 2),
-    (2, 3)])
+      (2, 3)])
 test([(2, 3), (1, 4), (2, 4), (1, 5), (1, 5), (2, 5), (1, 6), (1, 6), (1, 6),
-    (2, 6)])
+      (2, 6)])
 test([(1, 1), (2, 1), (1, 2), (1, 2), (2, 2), (2, 3), (1, 3), (2, 3), (2, 3),
-    (1, 4), (2, 4), (1, 4), (1, 4), (2, 4)])
+      (1, 4), (2, 4), (1, 4), (1, 4), (2, 4)])
