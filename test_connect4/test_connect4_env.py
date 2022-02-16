af2275a96600749a08edcb509f29bb9da5176c22
Martin Labe <martin@labe.fr>
2022-02-16 15:54:30 +0100

    [add] test the env

---
+++ test_connect4/test_connect4_env.py
@@ -0,0 +1,15 @@
+from gym_connect4.envs.connect4_env import Connect4Env
+
+
+k = 2
+n = 8
+a = Connect4Env({"width": 7, "height": 6, "connect": 4})
+for k in range(n):
+    print("Step", k)
+    player, action = 1 + k % 2, 1 + k % 2
+    obs, reward, done, info = a.step({player: action})
+    print(f"player: {player} , reward: {reward}")
+    if done["__all__"]:
+        print(f"# player {player} won.")
+        break
+a.render(mode="human")
