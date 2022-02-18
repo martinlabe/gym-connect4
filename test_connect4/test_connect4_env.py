b3816ca159e56d577480ba8524490e9e3d7205ef
Martin Labe <martin@labe.fr>
2022-02-18 17:59:26 +0100

    [add] see if the game is done

--- test_connect4/test_connect4_env.py
+++ test_connect4/test_connect4_env.py
@@ -2,7 +2,7 @@ from gym_connect4.envs.connect4_env import Connect4Env
 
 
 k = 2
-n = 8
+n = 10
 a = Connect4Env({"width": 7, "height": 6, "connect": 4})
 for k in range(n):
     print("Step", k)
@@ -10,6 +10,5 @@ for k in range(n):
     obs, reward, done, info = a.step({player: action})
     print(f"player: {player} , reward: {reward}")
     if done["__all__"]:
-        print(f"# player {player} won.")
-        break
+        print(f"# the game is done")
 a.render(mode="human")
