6a52f2b76b75da8c2c7d8a64bceeeca20c68a334
Martin Labe <martin@labe.fr>
2022-02-21 12:30:04 +0100

    [add] the reset function

--- test_connect4/test_connect4_env.py
+++ test_connect4/test_connect4_env.py
@@ -1,14 +1,20 @@
 from gym_connect4.envs.connect4_env import Connect4Env
+import matplotlib.pyplot as plt
 
-
-k = 2
-n = 10
 a = Connect4Env({"width": 7, "height": 6, "connect": 4})
-for k in range(n):
-    print("Step", k)
-    player, action = 1 + k % 2, 1 + k % 2
-    obs, reward, done, info = a.step({player: action})
-    print(f"player: {player} , reward: {reward}")
-    if done["__all__"]:
-        print(f"# the game is done")
-a.render(mode="human")
+
+
+def game(env, game, start, n):
+    """perform an alternate game"""
+    for k in range(start - 1, n + start):
+        player, action = 1 + k % 2, 1 + k % 2
+        obs, reward, done, info = a.step({player: action})
+        mes = "" if not done["__all__"] else "#done"
+        print(f"G{game}, Step {k} player {player} plays {action}, reward: {reward} {mes}")
+
+
+game(a, 1, 1, 10)
+a.print_board()
+a.reset()
+game(a, 2, 2, 10)
+a.print_board()
