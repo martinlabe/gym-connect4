5c413cfe11b22d4695e4bc20298fa3f46fbf8566
Martin Labe <martin@labe.fr>
2022-02-22 09:45:56 +0100

    [fix] verbose in test

--- test_connect4/test_connect4_env.py
+++ test_connect4/test_connect4_env.py
@@ -1,20 +1,26 @@
 from gym_connect4.envs.connect4_env import Connect4Env
 import matplotlib.pyplot as plt
 
-a = Connect4Env({"width": 7, "height": 6, "connect": 4})
+a = Connect4Env({"width": 7, "height": 6, "connect": 4, "verbose": True})
 
 
 def game(env, game, start, n):
     """perform an alternate game"""
-    for k in range(start - 1, n + start):
-        player, action = 1 + k % 2, 1 + k % 2
+    for k in range(n):
+        player, action = 1 + (k + start + 1) % 2, 1 + k % 2
         obs, reward, done, info = a.step({player: action})
-        mes = "" if not done["__all__"] else "#done"
-        print(f"G{game}, Step {k} player {player} plays {action}, reward: {reward} {mes}")
 
 
 game(a, 1, 1, 10)
 a.print_board()
 a.reset()
 game(a, 2, 2, 10)
+a.print_board()
+a.reset()
+a.step({1: 1})
+a.step({1: 1})
+a.step({2: 1})
+a.step({2: 1})
+a.step({1: 1})
+a.step({1: 1})
 a.print_board()
