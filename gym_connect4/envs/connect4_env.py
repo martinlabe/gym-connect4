669035f335e7fdcb0ba1304f645e9f5b3c2e2906
Martin Labe <martin@labe.fr>
2022-02-23 12:24:50 +0100

    [add] info

--- gym_connect4/envs/connect4_env.py
+++ gym_connect4/envs/connect4_env.py
@@ -20,25 +20,31 @@ class Connect4Env(MultiAgentEnv):
         :conf:   dict with the configuration parameters
         """
         # public
+        ## for the game
+        self.player = 0
+        self.done = False
+        self.state = ""
+        ## for the config
         self.type = np.bool
         self.size = (conf["height"], conf["width"])
         self.connect = conf["connect"]
+        ## for gym
         self.action_space = gym.spaces.Discrete(self.size[1])
         self.observation_space = gym.spaces.Box(low=False, high=True,
                                                 shape=(self.size[0],
                                                        self.size[1],
                                                        2),
                                                 dtype=self.type)
+        ## for modes
         self.verbose = False
         if 'verbose' in list(conf.keys()) and conf["verbose"]:
             self.verbose = True
         self.visualization = False
         if 'visualization' in list(conf.keys()) and conf["visualization"]:
             self.visualization = True
+
         # private
-        self.__player = 0
         self.__count = 0
-        self.__done = False
         self.__grid_p1 = np.zeros(self.size, dtype=self.type)
         self.__grid_p2 = np.zeros(self.size, dtype=self.type)
 
@@ -58,7 +64,7 @@ class Connect4Env(MultiAgentEnv):
         player, action = list(action_dict.items())[0]
         self.__count += 1
 
-        WIN, DRAWN, LOSS = 100, 0, -100,
+        WIN, DRAW, LOSS = 100, 0, -100,
         PLAY, WRONG, OVER = -1, -10, 0
         obs, rew, done, info = {}, \
                                {}, \
@@ -71,18 +77,20 @@ class Connect4Env(MultiAgentEnv):
                 print(f"Step {self.__count} actions: {action_dict}, reward: {rew} #{message}")
 
         # if the game is over
-        if self.__done:
+        if self.done:
             obs = {}
             rew[player] = OVER
             done["__all__"] = True
-            verbose(f"OVER: the game is over.")
+            info[player] = "OVER"
+            verbose(f"{info[player]}: the game is over.")
             return obs, rew, done, info
         # if it is not the turn
-        elif self.__player == player:
+        elif self.player == player:
             obs[self.get_other_player(player)] = self.board()
             rew[player] = WRONG
-            done["__all__"] = self.__done
-            verbose(f"WRONG: not the turn of player {player}")
+            done["__all__"] = self.done
+            info[player] = "WRONG"
+            verbose(f"{info[player]}: not the turn of player {player}")
             return obs, rew, done, info
         # else let's play
         position = self.play(player, action)
@@ -91,41 +99,46 @@ class Connect4Env(MultiAgentEnv):
         if position is None:
             # if it is because the grid is full
             if self.get_grid().all():
-                rew[player] = DRAWN
-                rew[self.get_other_player(player)] = DRAWN
+                rew[player] = DRAW
+                rew[self.get_other_player(player)] = DRAW
                 done["__all__"] = True
-                verbose(f"DRAWN: player {player} is facing a full grid.")
+                info[player] = "DRAW"
+                verbose(f"{info[player]}: player {player} is facing a full grid.")
             # if it is because of the rules
             else:
                 rew[player] = LOSS
                 done["__all__"] = True
-                verbose(f"WRONG: player {player} tried to play {action}.")
+                info[player] = "WRONG"
+                verbose(f"{info[player]}: player {player} tried to play {action}.")
         # if the action is legal
         else:
+            # update the last player
+            self.player = player
             # if it leads to wining
             if self.win(player, position):
                 rew[player] = WIN
                 rew[self.get_other_player(player)] = LOSS
                 done["__all__"] = True
-                verbose(f"WIN: player {player} won")
+                info[player] = "WIN"
+                verbose(f"{info[player]}: player {player} won")
             # if it's not
             else:
                 rew[player] = PLAY
                 done["__all__"] = False
-                self.__player = player
+                info[player] = "PLAY"
                 verbose("")
 
         # inform the environment if the game is done
-        self.__done = done["__all__"]
-
+        self.done = done["__all__"]
+        self.state = info[player]
         return obs, rew, done, info
 
     def reset(self):
         """restart the environment"""
         if self.visualization:
             self.to_string()
-        self.__done = False
-        self.__player = 0
+        self.done = False
+        self.player = 0
         self.__count = 0
         self.__grid_p1[:] = 0
         self.__grid_p2[:] = 0
