aa8b6736824b851ee5e14c9dfd4c4bf31e83d8b0
Martin Labe <martin@labe.fr>
2022-02-23 10:25:34 +0100

    [fix] multiagent mode

--- test_connect4/test_connect4_env.py
+++ test_connect4/test_connect4_env.py
@@ -1,26 +1,30 @@
 from gym_connect4.envs.connect4_env import Connect4Env
-import matplotlib.pyplot as plt
 
-a = Connect4Env({"width": 7, "height": 6, "connect": 4, "verbose": True})
+a = Connect4Env({"width": 7, "height": 6, "connect": 4,
+                 "verbose": True, "visualization": True})
 
 
-def game(env, game, start, n):
+def game(env, start, n):
     """perform an alternate game"""
     for k in range(n):
         player, action = 1 + (k + start + 1) % 2, 1 + k % 2
-        obs, reward, done, info = a.step({player: action})
+        env.step({player: action})
 
 
-game(a, 1, 1, 10)
-a.to_string()
+game(a, 1, 10)
 a.reset()
-game(a, 2, 2, 10)
-a.to_string()
+game(a, 2, 10)
 a.reset()
 a.step({1: 1})
-a.step({1: 1})
 a.step({2: 1})
-a.step({2: 1})
-a.step({1: 1})
-a.step({1: 1})
-a.to_string()
+a.step({1: 2})
+a.step({2: 2})
+a.step({1: 3})
+a.step({2: 3})
+a.step({1: 5})
+a.step({2: 5})
+a.step({1: 4})
+a.reset()
+
+
+
